1c20f65ad9f0ab05c8e7a3734f7bd46845bfefcc
thomazllr <guithomaz270@gmail.com>
2024-06-17 19:46:44 -0300

    OKAY

---
+++ main.c
@@ -0,0 +1,27 @@
+//
+// Created by thoma on 08/06/2024.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "CircularLinkedList.h"
+
+int main()
+{
+
+    CircList *List = CircList_create();
+    int soldados, pulos;
+
+    scanf("%d %d", &soldados, &pulos);
+
+    for (int i = 1; i <= soldados; i++)
+    {
+        CircList_add_last(List, i);
+    }
+
+    // CircList_print(List);
+
+    printf("%d\n", Josephus(List, pulos));
+
+    return 0;
+}
